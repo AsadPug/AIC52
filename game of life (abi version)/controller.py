b9b21a7ca08fe24cd63bed955e437f0c3c8aa542
Your Name <fournierabigail@outlook.com>
2023-08-26 18:34:55 -0400

    fixed stuff + new exemple

--- game of life (abi version)/controller.py
+++ game of life (abi version)/controller.py
@@ -1,12 +1,15 @@
+import sys 
+from PySide6.QtWidgets import QApplication
+
 from model import Grid
 from vue import Vue
-from time import sleep
 
-class Controller:
+class Controller(QApplication):
     def __init__(self, fps: int, width: int, height: int) -> None:
+        super().__init__(sys.argv)
         self.delay = (1/ fps)
         self.grid = Grid(width, height)
-        #self.vue = Vue(800, 800)
+        self.vue = Vue()
         self.is_game_over = False
 
     def start(self) -> None:
@@ -16,8 +19,8 @@ class Controller:
         while self.is_game_over is False :
             self.grid.update_grid()
             self.print_grid()
-            #self.vue.update_gui()
-            sleep(self.delay)
+            self.vue.update_gui()
+            
             
     #temporary printing
     def print_grid(self) -> None:
@@ -31,7 +34,11 @@ class Controller:
 
 def main():
     controller = Controller(2, 20, 20)
-    controller.start()
+    controller.exec()
 
 if __name__ == "__main__":
     main()
+
+
+#https://github.com/semk/GameOfLife/blob/master/gol/gol.py
+#game of life pyside ^^^^^^^^
--- game of life (abi version)/vue.py
+++ game of life (abi version)/vue.py
@@ -1,19 +1,16 @@
 import sys
-from PySide6 import QtCore, QtWidgets, QtGui
 
-class Vue:
+from PySide6.QtWidgets import QMainWindow, QApplication, QGraphicsView, QGraphicsScene
+from PySide6.QtGui import QBrush, QPen 
+from PySide6.QtCore import Qt, QTimer
+
+class Vue(QMainWindow):
     def __init__(self, w_width: int, w_height: int) ->None:
-        
-        gui = QtWidgets.QApplication([])
-        main_widget = QtWidgets.QWidget()
-        graphics_view = QtWidgets.QGraphicsView()
-        graphics_view.resize(w_width, w_height)
-        main_widget.resize(w_width, w_height)
-        rect = QtWidgets.QGraphicsRectItem(graphics_view)
-        rect
-        graphics_view.show()
-        #main_widget.show()
-        gui.exec()
+        scene = QGraphicsScene()
+        graphics_view = QGraphicsView()
+        self.geometry(0, 0, w_width, w_height)
+    
+        self.show()
 
     def update_gui(self) -> None:
         pass
